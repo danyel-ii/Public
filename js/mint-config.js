7e274cd5330f3a1fa8fbf01288ed626671ad3ae7
danyel-ii <222602034+danyel-ii@users.noreply.github.com>
2026-01-11 10:36:41 +0100

    Update Sepolia mint contract address

--- js/mint-config.js
+++ js/mint-config.js
@@ -1,7 +1,7 @@
 (() => {
   const configs = {
     sepolia: {
-      contractAddress: "0xd20E49e96df454174083baC7BAe8ccb483B3489e",
+      contractAddress: "0xf7B96E93D7E4b5aBf80E703Bb358E4Cb8aa53043",
       chainId: 11155111,
       chainName: "Sepolia",
       rpcUrl: "https://rpc.sepolia.org",
